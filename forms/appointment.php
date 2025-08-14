3e05b77ecdfe78b7ea5b2dad5763ecd2ae29efa7
Mahamatqulov <bmamatqulov615@gmailc.om>
2025-08-14 18:42:57 +0000

    Remove SMTP configuration comments from appointment form

--- forms/appointment.php
+++ forms/appointment.php
@@ -24,14 +24,14 @@
   $contact->subject = 'Online Appointment Form';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'Name');
   $contact->add_message( $_POST['email'], 'Email');
